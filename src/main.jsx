a6f19091a097805cd3e88eaa57948db53a61c26e
Iryna Kyryllova <keira.kirillova@gmail.com>
2024-10-17 17:14:27 +0200

    remove redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistor } from './redux/store'
 import { store } from './redux/store'
 import App from './App'
 import 'normalize.css'
@@ -11,9 +9,7 @@ import './index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </StrictMode>
 )
